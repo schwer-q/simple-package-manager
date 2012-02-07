9eef06ca935240672467c70f5a433c48b09befbf
cthug.zs@gmail.com <cthug.zs@gmail.com@a0a8e8af-1020-fe5a-0cf0-c6af36da8dd6>
2012-02-07 08:03:14 +0000

    Added the spm-accept utility.

---
+++ trunk/spm-accept.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# spm-accept - Accepts the licensing agreement when installing a package.
+# Copyright (C) 2011, Zachary Scott
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# accept the licensing agreement
+accept() {
+
+    echo "n" # don't view the license
+    echo "y" # accept the license terms and conditions
+
+    accept
+
+}
+
+accept
+
+# EOF
